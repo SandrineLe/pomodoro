06fd50d4391b479a6ecdb1b6faf67ef61eaf19c8
SandrineLe <le.sandrinemt@gmail.com>
2021-01-28 11:03:22 +0100

    maj index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {render} from "react-dom";
+
+class HelloMessage extends React.Component {
+    render() {
+        return <div>{`Hello ${this.props.name}`}</div>;
+    }
+}
+
+render(
+    <HelloMessage name={"Taylor"} />,
+    document.querySelector("#hello-example"),
+);
+
+console.log("yo");
+console.log("test");
+console.log("test");
